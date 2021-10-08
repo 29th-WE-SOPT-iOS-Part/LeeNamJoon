9295b6f3b5fc37092334367b0130d80b3fbcaf14
ningpop <craft1933@naver.com>
2021-10-09 01:09:59 +0900

    ➕[ADD] UI 완성

---
+++ Week1/week1-assignment/week1-assignment/CompleteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CompleteViewController.swift
+//  week1-assignment
+//
+//  Created by 이남준 on 2021/10/09.
+//
+
+import UIKit
+
+class CompleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
